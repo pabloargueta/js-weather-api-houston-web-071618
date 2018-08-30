8c723ff6ffb0e5b5ebe6e533557a2e9baf9aa39d
pabloargueta <pabloargueta27@gmail.com>
2018-08-30 16:48:28 -0500

    some new stuff

--- js/init.js
+++ js/init.js
@@ -8,8 +8,11 @@ stupidForm.addEventListener('submit', function (e) {
 function graphData () {
   let latitude = latitudeSelector.value
   let longitude = longitudeSelector.value
-
+  let address =
   const ctx = document.getElementById("WeatherChart").getContext("2d")
+  const Google_API_KEY = 'AIzaSyC3fQ7Z_mSmslLBYuQTYbz_gsFhcHDI6eY'
+  const Google_URL = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=" + Google_API_KEY;
+
   const API_KEY = "54750043e7dedd710de09d777236c338";
   const CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
   const URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/" + latitude + "," + longitude + "?exclude=currently,minutely,daily";
