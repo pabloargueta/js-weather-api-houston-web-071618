b881404029f92e34f8c210687c592c0a77e9eeda
Jeannie Kim <SJ.Jeannie.Kim@gmail.com>
2018-08-30 17:33:50 -0500

    fixed

--- js/init.js
+++ js/init.js
@@ -1,29 +1,38 @@
 let stupidForm = document.getElementById('coordinates-form');
-let submitButton = document.getElementById('submitButton').addEventListener('click', graphData);
+let submitButton = document
+  .getElementById('submitButton')
+  .addEventListener('click', graphData);
 let latitudeSelector = document.getElementById('latitude');
 let longitudeSelector = document.getElementById('longitude');
-stupidForm.addEventListener('submit', function (e) {
-  e.preventDefault()
-})
-function graphData () {
-  let latitude = latitudeSelector.value
-  let longitude = longitudeSelector.value
-  let address =
-  const ctx = document.getElementById("WeatherChart").getContext("2d")
-  const Google_API_KEY = 'AIzaSyC3fQ7Z_mSmslLBYuQTYbz_gsFhcHDI6eY'
-  const Google_URL = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=" + Google_API_KEY;
+stupidForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+});
+function graphData() {
+  let latitude = latitudeSelector.value;
+  let longitude = longitudeSelector.value;
+  // let address =
+  const ctx = document.getElementById('WeatherChart').getContext('2d');
+  // const Google_API_KEY = 'AIzaSyC3fQ7Z_mSmslLBYuQTYbz_gsFhcHDI6eY'
+  // const Google_URL = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=" + Google_API_KEY;
 
-  const API_KEY = "54750043e7dedd710de09d777236c338";
-  const CORS_WRAPPER = "https://cors-anywhere.herokuapp.com/"
-  const URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/" + latitude + "," + longitude + "?exclude=currently,minutely,daily";
+  const API_KEY = '54750043e7dedd710de09d777236c338';
+  const CORS_WRAPPER = 'https://cors-anywhere.herokuapp.com/';
+  const URL =
+    CORS_WRAPPER +
+    'https://api.darksky.net/forecast/' +
+    API_KEY +
+    '/' +
+    latitude +
+    ',' +
+    longitude +
+    '?exclude=currently,minutely,daily';
 
   // const URL = CORS_WRAPPER + "https://api.darksky.net/forecast/" + API_KEY + "/40.7127,-74.0059?exclude=currently,minutely,daily";
 
   // make your fetch in the makeRequest function in weatherChart.js
-  makeRequest(URL, ctx)
+  makeRequest(URL, ctx);
 
   //add a form to make map location dynamic
-
 }
 
 // submitButton.addEventListener('click', graphData());
@@ -32,7 +41,7 @@ document.addEventListener('DOMContentLoaded', graphData);
 // ((result.hourly.data[0].time)*1000);
 //
 // result.hourly.data.map do |obj|
-//   obj.time*100
+//   obj.time*1000
 // end
 
 // const hours = result.hourly.data.map (x => x * 1000);
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,26 +1,26 @@
-function getFahrenheit(result){
-  return result.hourly.data.map (
-    function(item) {
-       return (item.temperature);
-  })
+function getFahrenheit(result) {
+  return result.hourly.data.map(function(item) {
+    return item.temperature;
+  });
 }
 
 // function map(array, callback) {
-//   const newArray = []
+//   const newArray = [];
 //   for (const item of array) {
-//     newArray.push(callback(item))
+//     newArray.push(callback(item));
 //   }
-//   return newArray
+//   return newArray;
 // }
-// let dataArray = result.hourly.data
-// map(dataArray, function(item) {
-//   return item.temperature
-// })
 
-function getHour(result){
-    return result.hourly.data.map ( function(item) {
-      return new Date(item.time * 1000).getHours();
-    })
+// let dataArray = result.hourly.data;
+// map(dataArray, function(item) {
+//   return item.temperature;
+// });
+
+function getHour(result) {
+  return result.hourly.data.map(function(item) {
+    return new Date(item.time * 1000).getHours();
+  });
 }
 
 function generateDataSet(labels, data) {
@@ -30,9 +30,9 @@ function generateDataSet(labels, data) {
       labels: labels,
       datasets: [
         {
-          label: "NYC Weather Data",
+          label: 'Weather Data',
           data: data,
-          backgroundColor: "rgba(100,150,220,0.2)",
+          backgroundColor: 'rgba(100,150,220,0.2)',
           borderColor: 'rgb(255, 99, 132)'
         }
       ]
@@ -40,26 +40,24 @@ function generateDataSet(labels, data) {
     options: {
       // additional configurations here
     }
-  }
+  };
 }
 
 function makeRequest(endpoint, canvas) {
   //Get the weather data using a [fetch request](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch).
 
   return fetch(endpoint)
-    .then( res => res.json() )
-    .then( hourlyData => {
-      console.log(hourlyData);
+    .then((res) => res.json())
+    .then((hourlyData) => {
+      // console.log(hourlyData);
 
       let hours = getHour(hourlyData);
       let temp = getFahrenheit(hourlyData);
       let dataset = generateDataSet(hours, temp);
 
-
-
       const tempChart = new Chart(canvas, dataset);
-    })
+    });
 
-//   // After your fetch works - use your json data with the line below :)
-//    const tempChart = new Chart(canvas, generateDataSet(getHour(hourlyData), getFahrenheit(hourlyData)))
+  //   // After your fetch works - use your json data with the line below :)
+  //    const tempChart = new Chart(canvas, generateDataSet(getHour(hourlyData), getFahrenheit(hourlyData)))
 }
